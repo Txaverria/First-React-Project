50afd86db5f3983f112ec4a062f30b4e60c67e03
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-09-12 06:41:33 -0600

    Add CoreConcept Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
+import propsImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
+
+export const CORE_CONCEPTS = [
+  {
+    image: componentsImg,
+    title: "Components",
+    description:
+      "The core UI building block - compose the user interface by combining multiple components.",
+  },
+  {
+    image: jsxImg,
+    title: "JSX",
+    description:
+      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+  },
+  {
+    image: propsImg,
+    title: "Props",
+    description:
+      "Make components configurable (and therefore reusable) by passing input data to them.",
+  },
+  {
+    image: stateImg,
+    title: "State",
+    description:
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+  },
+];
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +52,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -36,15 +66,16 @@ function App() {
   return (
     <div>
       <Header />
-      <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <CoreConcept
-          title="Components"
-          description="The core UI building block."
-          image={componentsImg}
-        />
-      </section>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
