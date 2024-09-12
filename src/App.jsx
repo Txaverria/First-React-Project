60fe5752085ec82337b74e2f14545d90ec3619ec
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-09-12 06:51:15 -0600

    Separate components from App.jsx into components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
-import propsImg from "./assets/config.png";
-import jsxImg from "./assets/jsx-ui.png";
-import stateImg from "./assets/state-mgmt.png";
-
-export const CORE_CONCEPTS = [
-  {
-    image: componentsImg,
-    title: "Components",
-    description:
-      "The core UI building block - compose the user interface by combining multiple components.",
-  },
-  {
-    image: jsxImg,
-    title: "JSX",
-    description:
-      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
-  },
-  {
-    image: propsImg,
-    title: "Props",
-    description:
-      "Make components configurable (and therefore reusable) by passing input data to them.",
-  },
-  {
-    image: stateImg,
-    title: "State",
-    description:
-      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
-  },
-];
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const randomDescription = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {randomDescription} Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from "./data.jsx";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
