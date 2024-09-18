8275d06c47872f1c8c9aa6ccd7c18de26132f0f5
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-09-18 09:05:08 -0600

    Include dynamic useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,25 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function tabClicked(selectedTab) {
     setSelectedTopic(selectedTab);
   }
 
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -32,15 +45,7 @@ function App() {
             <TabButton onSelect={() => tabClicked("props")}>Components</TabButton>
             <TabButton onSelect={() => tabClicked("state")}>Components</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
