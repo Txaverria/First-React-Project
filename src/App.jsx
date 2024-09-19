f710e688a8a60d2d46b225b3925f5cbccdb0b53a
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-09-19 06:45:19 -0600

    Divide App in different components

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,15 @@ import { EXAMPLES } from "./data";
 import Header from "@components/Header/Header";
 import CoreConcepts from "@components/CoreConcepts";
 import TabButton from "@components/TabButton";
+import Examples from "@components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function tabClicked(selectedTab) {
-    setSelectedTopic(selectedTab);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
   return (
     <>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => tabClicked("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => tabClicked("jsx")}>
-              JSX
-            </TabButton>
-            <TabButton isSelected={selectedTopic === "props"} onSelect={() => tabClicked("props")}>
-              Props
-            </TabButton>
-            <TabButton isSelected={selectedTopic === "state"} onSelect={() => tabClicked("state")}>
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <Examples />
       </main>
     </>
   );
