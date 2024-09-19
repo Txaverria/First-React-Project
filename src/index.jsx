f855f27ee8f194694ec69c8f2048705dca97c644
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-09-19 06:57:16 -0600

    update index imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import './index'
+import './index.css'
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
